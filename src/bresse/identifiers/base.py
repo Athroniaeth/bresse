3bb1b263b25bdd25611c2e1a37ee7f55dceec7eb
Athroniaeth <pierre.chaumont@hotmail.fr>
2024-08-23 02:20:35 +0200

    Create Skeleton for 'identifiers'

---
+++ src/bresse/identifiers/base.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class ModelId:
+    """
+    Model identifier purpose by company
+
+    Attributes:
+        id (str): Model identifier (ex: 'gpt-3.5-turbo')
+        input_cost_million (int): Cost in million for input (in $)
+        output_cost_million (int): Cost in million for output (in $)
+    """
+    id: str
+    input_cost_million: int
+    output_cost_million: int
---
+++ src/bresse/identifiers/openai.py
@@ -0,0 +1,10 @@
+from dataclasses import dataclass
+
+from bresse.identifiers.base import ModelId
+
+
+@dataclass
+class GPT35Turbo(ModelId):
+    id: str = 'gpt-3.5-turbo'
+    input_cost_million: int = 3
+    output_cost_million: int = 6
