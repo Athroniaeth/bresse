ef50b9a6163047b86db25d755eecf26afec6083e
Athroniaeth <pierre.chaumont@hotmail.fr>
2024-08-27 13:34:43 +0200

    Add tests for OpenAI models

---
+++ tests/integrations/models/test_model.py
@@ -0,0 +1,34 @@
+import os
+from pathlib import Path
+
+import pytest
+from dotenv import load_dotenv
+
+from bresse.models.openai import OpenAIModel
+from bresse.output import Output
+from bresse.result import CounterResult
+from tests.conftest import load_path_pgn
+
+load_dotenv()
+
+
+@pytest.mark.cost
+@pytest.mark.parametrize("path_pgn", load_path_pgn())
+def test_models_inference(path_pgn: Path) -> None:
+    pgn = path_pgn.read_text()
+
+    model = OpenAIModel(
+        model_id="gpt-3.5-turbo-instruct",
+        api_key=os.getenv("OPENAI_API_KEY"),
+    )
+    output, counter = model.inference(pgn)
+
+    assert isinstance(output, Output)
+    assert output.model_id == model.model
+
+    assert output.number_requests == 1
+    # assert output.inputs_tokens == ... # Depends on the PGN
+
+    assert isinstance(counter, CounterResult)
+    print()
+    # Result depends on Board (and therefore PGN, test only the type)
