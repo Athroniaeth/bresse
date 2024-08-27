9432d98efc90422c438c951bf4742fffa484eb8e
Athroniaeth <pierre.chaumont@hotmail.fr>
2024-08-27 12:38:01 +0200

    Add tests models on FakeModel (tests asbract class)

---
+++ tests/integrations/test_models.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+
+import pytest
+
+from bresse.output import Output
+from bresse.result import CounterResult
+from conftest import load_path_pgn, FakeModel
+
+
+@pytest.mark.parametrize("path_pgn", load_path_pgn())
+def test_models_inference(path_pgn: Path) -> None:
+    pgn = path_pgn.read_text()
+    model = FakeModel(model_id="gpt-3.5-turbo-instruct")
+    output, counter = model.inference(pgn)
+
+    assert isinstance(output, Output)
+    assert output.model_id == model.model
+
+    assert output.number_requests == 1
+    assert output.outputs_tokens == 3
+    # assert output.inputs_tokens == ... # Depends on the PGN
+
+    assert isinstance(counter, CounterResult)
+    print()
+    # Result depends on Board (and therefore PGN, test only the type)
